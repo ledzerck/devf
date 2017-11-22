960653a6a48b43d8b156413d436a3e7d26b54fdf
ledzerck <aldomapler@gmail.com>
2017-11-21 20:28:58 -0600

    Aquí va Menid :D

--- flaskTemplates/app.py
+++ flaskTemplates/app.py
@@ -1,6 +1,11 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify, render_template
 from flask import request
 import json
+
+
+
+# Es común llamarlo app.py
+#
 
 # Con esto creamos el objeto flask
 app = Flask(__name__)
@@ -28,7 +33,7 @@ def index():
 # Necesita la e
 @app.errorhandler(404)
 def page_not_found(e):
-    return "Esta página no existe :("
+    return render_template('page.html')
 
 # Podemos indicar que tipo de peticiones va a aceptar
 @app.route('/tareas', methods = ['GET', 'POST'])
@@ -40,20 +45,6 @@ def tareas():
         lista_tareas = []
         lista_tareas.append(body['tareas'])
         return jsonify({'tareas_realiza':lista_tareas})
-
-@app.route('/params')
-def params():
-    # Con esto le indicamos cuales son las variables que podemos recibir en la URI
-    parametro = request.args.get('params1','valor por default')
-    parametro2 = request.args.get('params2','valor por default')
-    print (parametro)
-    print (parametro2)
-    return "El parametro es: {}, {}".format(parametro,parametro2)
-
-# Podemos convertir la variable a otro tipo <int:name>
-@app.route('/user/<name>/')
-def user(name):
-    return "Los parámetros por path son: {}".format(name)
 
 # Hay que hacer una confirmación:
 if __name__ == '__main__':
